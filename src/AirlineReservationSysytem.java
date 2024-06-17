3836c076c85ea34b7e4bfe7b783755762aecfef2
Cuma TALJIBINI <taljibinicuma@gmail.com>
2024-06-18 00:29:16 +0300

    added ReaderThread and organizing the Maine file

--- src/AirlineReservationSysytem.java
+++ src/AirlineReservationSysytem.java
@@ -1,28 +1,24 @@
-import java.nio.channels.WritableByteChannel;
+
 import java.util.concurrent.Semaphore;
 
 /**
  * Methods
  */
 public class AirlineReservationSysytem implements Runnable{
-    private static final int MAX_CONCURRENT_WRITERS = 1;  // Maksimum aynı anda yazan sayısı.  
-    private static final int MAX_CONCURRENT_READERS = 1;  // Maksimum aynı anda okuyna sayısı.
 
     private Semaphore writerSemaphore;
     private Semaphore readerSemaphore;
     
     public int userID;
-    public int requestSead; // istenen kultuk numaraı.
-    public int orderOfOperation; // işlem sırası.
+    public int requestSead; // istenen kultuk numarsı.
     public String seatStatus;
     public int[] seats;
 
-    public AirlineReservationSysytem(Semaphore writerSemaphore , Semaphore readerSemaphore, int userID, int requestSead, int orderOfOperation, int[] seats ){
+    public AirlineReservationSysytem(Semaphore writerSemaphore , Semaphore readerSemaphore, int userID, int requestSead, int[] seats ){
         this.writerSemaphore = writerSemaphore;
         this.readerSemaphore = readerSemaphore;
         this.userID = userID;
         this.requestSead = requestSead;
-        this.orderOfOperation = orderOfOperation;
         this.seats = seats;
     }
     
@@ -31,7 +27,7 @@ public class AirlineReservationSysytem implements Runnable{
         try{
             writerSemaphore.acquire(); // Yazar semaforunu elde et.
             System.out.println("Time: "+ java.time.LocalTime.now());
-            System.out.println("Writer "+ userID + " tire to book the seat" + requestSead);
+            System.out.println("Writer "+ userID + " tire to book the seat " + requestSead);
 
             if(seats[requestSead] == 0){ // Kolduk durumu 0 ise (boş) satın al ve durumu 1'e çak.
                 seats[requestSead] = 1;  // o kultuğu durumu 1 olduğundan başka bir writer tarafından alınamaz durumdadır.
@@ -39,21 +35,46 @@ public class AirlineReservationSysytem implements Runnable{
 
             }else if(seats[requestSead] == 1){
                 seatStatus = "bookedUp";
-                System.out.println("Writer "+ userID + "could not booked seat number" + requestSead + " since it has been already booked"); 
+                System.out.println("Writer "+ userID + " could not booked seat number" + requestSead + " since it has been already booked"); 
             }
             System.out.println("-------------------------------------------");
+            ReaderThread(); // Her write işlemden sora Reader son durumu görmek için Reader() fonksiyonu burda yerleştridim.
 
         }catch(InterruptedException e){
             e.printStackTrace();
         }finally{
           writerSemaphore.release(); // Semaforu serbest bırak.
         }
+
+    }
+
+    // Reader
+    private void ReaderThread() {
+        try{  
+            readerSemaphore.acquire(); // Okuyucu semaforunu elde et
+            System.out.println("Time: "+ java.time.LocalTime.now());
+            System.out.println("Reader "+ userID + " looks for available seats. State of the seat are: ");
+            for(int i = 0; i < seats.length; i++){
+                System.out.println("seat No " + i + " : " + seats[i]);
+            }
+            System.out.println("\n"+"-------------------------------------");
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+
+        }finally{
+            readerSemaphore.release();   // Semaforu serbest bırak.
+        }
     }
 
     @Override
     public void run() {
-      // TODO Auto-generated method stub
-      throw new UnsupportedOperationException("Unimplemented method 'run'");
+      try {
+          makeReservation(); // Koltuğu al ve ardından bilet durumunu göster
+          Thread.sleep(500);
+      }catch (Exception e) {
+          e.printStackTrace();
+      }
     }
 
 
--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,29 @@
+import java.util.concurrent.Semaphore;
 /**
  * Main
  */
-
-
-/**
- * InnerMain
- */
-class InnerMain {
-
-  
-}
-
-
-
-
-
-
 public class Main {
+    private static final int MAX_CONCURRENT_WRITERS = 1;  // Maksimum aynı anda yazan sayısı.  
+    private static final int MAX_CONCURRENT_READERS = 1;  // Maksimum aynı anda okuyna sayısı.
+
+    public static void main(String[] args) throws InterruptedException {
+        
+        int[] seats = new int[5];
+        Semaphore writerSemaphore = new Semaphore(MAX_CONCURRENT_WRITERS);
+        Semaphore readerSemaphore = new Semaphore(MAX_CONCURRENT_READERS);
+
+        AirlineReservationSysytem request1 = new AirlineReservationSysytem(writerSemaphore, readerSemaphore, 0, 0, seats);
+        AirlineReservationSysytem request2 = new AirlineReservationSysytem(writerSemaphore, readerSemaphore, 1, 1, seats);
+        AirlineReservationSysytem request3 = new AirlineReservationSysytem(writerSemaphore, readerSemaphore, 1, 1, seats);
+
+        Thread thread1 = new Thread(request1);
+        Thread thread2 = new Thread(request2);
+        Thread thread3 = new Thread(request3);
+
+        thread1.start();
+        thread2.start();
+        thread3.start();
 
 
-
-  public static void main(String[] args) {
-
-  }
+    }
 }
